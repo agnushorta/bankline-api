ac699d7e179d19d66015e0cf384520f3712accce
Agnus Horta <agnus.horta@outlook.com>
2022-05-07 10:34:21 -0300

    mapeando as minhas entidades

--- src/main/java/com/dio/santander/bankline/api/model/Conta.java
+++ src/main/java/com/dio/santander/bankline/api/model/Conta.java
@@ -1,7 +1,15 @@
 package com.dio.santander.bankline.api.model;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
 public class Conta {
+	
+	@Column(name = "conta_numero")
 	private Long numero;
+	
+	@Column(name = "conta_saldo")
 	private Double saldo;
 	public Long getNumero() {
 		return numero;
@@ -15,6 +23,5 @@ public class Conta {
 	public void setSaldo(Double saldo) {
 		this.saldo = saldo;
 	}
-	
 	
 }
